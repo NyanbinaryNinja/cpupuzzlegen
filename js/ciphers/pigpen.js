a15fe224ad626138b39db2899f89e1658736df79
NyanbinaryNinja <58090946+NyanbinaryNinja@users.noreply.github.com>
2026-03-23 23:05:36 +0000

    centered pigpen text

--- js/ciphers/pigpen.js
+++ js/ciphers/pigpen.js
@@ -20,6 +20,7 @@ class pigpen_cipher {
 
   draw(colors) {
     this.ctx.textAlign = 'center';
+    this.ctx.textBaseline = 'middle';
     this.ctx.strokeStyle = colors.c_main;
     this.ctx.fillStyle = colors.c_main;
     this.ctx.lineWidth = 2;
